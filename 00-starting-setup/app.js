541f764a32a3b5dc4ebd6e3e9d055022bbf8d699
Parker Sheehan <Parkersheehan007@gmail.com>
2023-04-14 13:24:12 -0500

    sequlize

--- 00-starting-setup/app.js
+++ 00-starting-setup/app.js
@@ -15,6 +15,9 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -37,6 +40,12 @@ app.use(errorController.get404);
 Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
 
+User.hasOne(Cart);
+Cart.belongsTo(User);
+
+Cart.belongsToMany(Product, {through: CartItem});
+Product.belongsToMany(Cart, {through: CartItem});
+
 sequelize
   .sync()
   .then((result) => {
@@ -49,6 +58,9 @@ sequelize
     return user;
   })
   .then((user) => {
+    user.createCart()
+  })
+  .then(() => {
     app.listen(3000);
   })
   .catch((err) => {
